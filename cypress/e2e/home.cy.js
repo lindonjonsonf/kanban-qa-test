05a5a80b914ac17c0ffb6ec251940862ccc1bc41
Lindonjonson Filho <lindojhonsonf@gmail.com>
2025-08-12 18:15:57 -0300

    Adicionando teste para excluir um item da lista criada

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -9,4 +9,10 @@ describe('Kanban - Fluxos principais', () => {
     cy.get('.btn').click();
     cy.contains('.board-header-title', 'teste').should('be.visible');
   });
+   it('Deve excluir uma lista', () => {
+    cy.contains('Adicionar outra lista').click();
+    cy.get('.sc-gsnTZi').type('teste');
+    cy.get('.btn').click();
+    cy.get('#testetrash svg').click();
+  });
 });
